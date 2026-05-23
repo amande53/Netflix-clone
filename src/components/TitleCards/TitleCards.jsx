1bf4b9afd06d8925f0e1abb52f36907ceca3432f
Amanda Anderson <amanderson9@yahoo.com>
2026-05-23 06:41:19 -0400

    Navbar complete

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCards'
+
+const TitleCards = () => {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,12 @@
+import.'/Player.css'
+import React from 'react'
+
+const Player = () => {
+  return (
+    <div className="player">
+      
+    </div>
+  )
+}
+
+export default Player
